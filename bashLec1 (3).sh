8a6df2983183bf7adced6777934876d9616c581e
yara121199 <88503735+yara121199@users.noreply.github.com>
2023-01-01 20:28:34 +0200

    Add files via upload

---
+++ bashLec1 (3).sh
@@ -0,0 +1,250 @@
+#!/usr/bin/bash
+export LC_COLLATE=C
+shopt -s extglob
+#sed -n '/lp/p' /etc/passwd
+#sed -n '3!p' /etc/passwd
+#3---
+#sed -n '/lp/!p' /etc/passwd
+#5----------
+#awk -F':' '
+#BEGIN {}
+#{print $5;}
+#END{print "END";}' /etc/passwd
+
+#awk -F':' '
+#BEGIN {}
+#print $5;
+#}
+#END{print "END";}' /etc/passwd
+
+#awk -F':' '
+#BEGIN {}
+#{
+#if($3>500){print $3;
+#print $5;
+#}}
+#END{print "END";}' /etc/passwd
+
+
+#awk -F':' '
+#BEGIN {}
+#{if($3==500){print $3;
+#print $5;
+#}}
+#END{print "END";}' /etc/passwd
+
+#awk -F':' '
+#BEGIN {}
+#{if(NR>5&&NR<15){print $0;}}
+#END{print "END";}' /etc/passwd
+#6-----------
+#awk -F':' '
+#BEGIN {
+#line=""
+#max=0}
+#{if($3>max){max=$3
+#line=$0
+#}}
+#END{print line;}' /etc/passwd
+
+
+#awk -F':' '
+#BEGIN {
+#count=0}
+#{count=count+$3
+#}
+#END{print count;}' /etc/passwd
+
+#awk -F':' '
+#BEGIN { getline m_gid; sum_uid=0 }
+#{
+#if($4==m_gid)
+ # sum_uid += $3
+  #}
+#END { printf "Sum of UID = %d", sum_uid }' /etc/#passwd
+#alias c="cd /DB"
+
+rej='^[A-Z | a-z][A-Za-z0-9]+$'
+if [ -d "DB" ];then
+      cd ./DB
+      echo "Your are connected to the engine now"
+  else
+    mkdir ./DB
+    cd ./DB
+    echo "Your are connected weweqeqewto the engine now"
+fi
+while true
+do
+  select choice in "Create DB"  "List DB"  "Drop DB"  "Connect DB"
+   do
+     case $choice in
+       "Create DB")
+             read -p "Enter Your DB name " db_name
+          if [ -d $db_name ];then
+                echo "Your DB is already exist"
+                break
+          fi
+          if [[ ${db_name} =~ $rej ]]
+          then
+            mkdir ./$db_name
+            echo "Your DB is created Successfully"
+          else
+            echo "Your Db name is invalid you should start with letters and it only contains small and capital and numbers only"
+
+          fi
+
+       ;;
+      "List DB")
+       ls -a
+      ;;
+      "Drop DB")
+          read -p "Enter Your DB name " db_name
+          if [ -d $db_name ];then
+                rm -r $db_name
+                echo "your DB has Successfully droped"
+            else
+              echo "your DB is not exist"
+          fi
+      echo
+      ;;
+      "Connect DB")
+        read -p "Enter Your DB name " db_name
+          if [ -d $db_name ];then
+           cd $db_name
+              while true
+                   do
+                    select choice in "Create Table"  "Insert Table"  "Drop Table"  "Select Table" "Delete Table" "Update Table" "List Table"
+                      do
+                        case $choice in
+                        "Create Table")
+                                  read -p "Enter your table name " tb_name
+                                  if [ -f $tb_name ];then
+                                        echo "Your table is already exist"
+                                        break
+                                  fi
+                                  if [[ ${tb_name} =~ $rej ]]
+                                  then
+                                    touch ./$tb_name
+                                    read -p "please enter number of fields" num_fields
+                                    echo "Please make sure that the first field is the PRIMARY KEY"
+                                   arr=()
+                                   dataTypeArr=()
+                                   declare -i i=0
+                                    while ((i < num_fields))
+                                    do
+                                      read -p "please enter field name " field_name
+                                      name=$field_name":"
+                                      read -p "please enter field Data Type " dataType_name
+                                      type=$dataType_name":"
+                                       arr+=$name
+                                       dataTypeArr+=$type
+                                       ((i=$i+1))
+                                    done
+                                    echo ${arr[0]} >> ./$tb_name
+                                    echo ${dataTypeArr[0]} >> ./$tb_name
+                                    echo "Your table is created Successfully"
+                                  else
+                                    echo "Your table name is invalid you should start with letters and it only contains small and capital and numbers only"
+
+                                  fi
+                        ;;
+                        "Insert Table")
+                          read -p "Enter your table name " tb_name
+                          if [ -f $tb_name ];then
+                            data=()
+                            declare -i index=0
+                             while ((index < `head -n1 ./$tb_name | grep -o ":" | wc -l`))
+                             do
+                               read -p "please insert data of col ${index} " col_value
+                               dt=$col_value":"
+                                data+=$dt
+                                ((index=$index+1))
+                             done
+                             echo ${data[0]} >> ./$tb_name
+                          else
+                            echo "you havn't a table with this name"
+                          fi
+
+                          ;;
+                        "Drop Table")
+                          read -p "Enter your table name " tb_name
+                          if [ -f $tb_name ];then
+                             rm  tb_name
+                          else
+                            echo "you havn't a table with this name"
+                          fi
+                          ;;
+                        "Select Table")
+                         read -p "Enter your table name " tb_name
+                        if [ -f $tb_name ];then
+                          while true
+                            do
+                              select choice in "All"  "Field by primary key"  "Column" "Exit"
+                                do
+                                  case $choice in
+                                    "All")
+                                    cat ./$tb_name
+                                    ;;
+                                    "Field by primary key")
+                                    ;;
+                                    "Column")
+                                    read -p "Enter your column name " col_name
+                                      line=$(head -n 1 $tb_name)
+                                      IFS=':' read -r -a ar <<< "$line"
+                                      declare -i index=0
+                                      declare -i in=0
+                                      for i in "${ar[@]}"
+                                      do
+                                        ((in=$in+1))
+                                        if [[ "${i}" == "${col_name}" ]]
+                                          then
+                                           ((index = $in))
+                                        fi
+                                      done
+                                       cut -d : -f$index $tb_name
+                                    ;;
+                                    "Exit")
+                                    break
+                                    ;;
+                                    *)
+                                    echo "Please choose a correct answer"
+                                  esac
+                                done
+                            done
+                          else
+                            echo "you havn't a table with this name"
+                        fi
+                          ;;
+                        "Delete Table")
+                           read -p "Enter your table name " tb_name
+                          if [ -f $tb_name ];then
+                           echo -n '' > $tb_name
+                          else
+                            echo "you havn't a table with this name"
+                          fi
+                          ;;
+                        "Update Table")
+                          echo
+                          ;;
+                        "List Table")
+                          ls -a
+                          ;;
+                         *)
+                           "Please Choose a correct answer"
+                      esac
+                    done
+                done
+            else
+            echo "your DB is not exist"
+            fi
+      ;;
+      *)
+        echo "Please Enter A Correct Choice"
+        ;;
+      esac
+    done
+done
+
+
+
+
---
+++ bashLec1.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/bash
+export LC_COLLATE=C
+shopt -s extglob
+#sed -n '/lp/p' /etc/passwd
+#sed -n '3!p' /etc/passwd
+#3---
+#sed -n '/lp/!p' /etc/passwd
+#5----------
+#awk -F':' '
+#BEGIN {}
+#{print $5;}
+#END{print "END";}' /etc/passwd
+
+#awk -F':' '
+#BEGIN {}
+#print $5;
+#}
+#END{print "END";}' /etc/passwd
+
+#awk -F':' '
+#BEGIN {}
+#{
+#if($3>500){print $3;
+#print $5;
+#}}
+#END{print "END";}' /etc/passwd
+
+
+#awk -F':' '
+#BEGIN {}
+#{if($3==500){print $3;
+#print $5;
+#}}
+#END{print "END";}' /etc/passwd
+
+#awk -F':' '
+#BEGIN {}
+#{if(NR>5&&NR<15){print $0;}}
+#END{print "END";}' /etc/passwd
+#6-----------
+#awk -F':' '
+#BEGIN {
+#line=""
+#max=0}
+#{if($3>max){max=$3
+#line=$0
+#}}
+#END{print line;}' /etc/passwd
+
+
+#awk -F':' '
+#BEGIN {
+#count=0}
+#{count=count+$3
+#}
+#END{print count;}' /etc/passwd
+
+#awk -F':' '
+#BEGIN { getline m_gid; sum_uid=0 }
+#{
+#if($4==m_gid)
+ # sum_uid += $3
+  #}
+#END { printf "Sum of UID = %d", sum_uid }' /etc/#passwd
+#alias c="cd /DB"
+
+rej='^[A-Z | a-z][A-Za-z0-9]+$'
+if [ -d "DB" ];then
+      cd ./DB
+      echo "Your are connected to the engine now"
+  else
+    mkdir ./DB
+    cd ./DB
+    echo "Your are connected weweqeqewto the engine now"
+fi
+while true
+do
+  select choice in "Create DB"  "List DB"  "Drop DB"  "Connect DB"
+   do
+     case $choice in
+       "Create DB")
+             read -p "Enter Your DB name " db_name
+          if [ -d $db_name ];then
+                echo "Your DB is already exist"
+                break
+          fi
+
+          if [[ ${db_name} =~ $rej ]]
+          then
+            mkdir ./$db_name
+            echo "Your DB is created Successfully"
+          else
+            echo "Your Db name is invalid you should start with letters and it only contains small and capital and numbers only"
+
+          fi
+
+       ;;
+      "List DB")
+       ls -a
+      ;;
+      "Drop DB")
+          read -p "Enter Your DB name " db_name
+          if [ -d $db_name ];then
+                rm -r $db_name
+                echo "your DB has Successfully droped"
+            else
+              echo "your DB is not exist"
+          fi
+      echo
+      ;;
+      "Connect DB")
+        read -p "Enter Your DB name " db_name
+          if [ -d $db_name ];then
+           cd $db_name
+              while true
+                   do
+                    select choice in "Create Table"  "Insert Table"  "Drop Table"  "Select Table" "Delete Table" "Update Table" "List Table"
+                      do
+                        case $choice in
+                        "Create Table")
+                                  read -p "Enter your table name " tb_name
+                                  if [ -f $tb_name ];then
+                                        echo "Your table is already exist"
+                                        break
+                                  fi
+
+                                  if [[ ${tb_name} =~ $rej ]]
+                                  then
+                                    touch ./$tb_name
+                                    echo "Your table is created Successfully"
+                                  else
+                                    echo "Your table name is invalid you should start with letters and it only contains small and capital and numbers only"
+
+                                  fi
+                        ;;
+                        "Insert Table")
+                          echo
+                          ;;
+                        "Drop Table")
+                          read -p "Enter your table name " tb_name
+                          rm -r tb_name
+                          ;;
+                        "Select Table")
+                          read -p "Enter your table name " tb_name
+                          cat tb_name
+                          ;;
+                        "Delete Table")
+                          if [ -f $tb_name ];then
+                          echo -n '' > tb_name
+                          else
+                            echo "you havn't a table with this name"
+                          fi
+                          ;;
+                        "Update Table")
+                          echo
+                          ;;
+                        "List Table")
+                          ls -a
+                          ;;
+                         *)
+                           "Please Choose a correct answer"
+                      esac
+                    done
+                done
+            else
+            echo "your DB is not exist"
+            fi
+      ;;
+      *)
+        echo "Please Enter A Correct Choice"
+        ;;
+      esac
+    done
+done
+
+
+
+
